8e0e038ae779808252e63b9414a321a03701044f
unknown <mark@rowra.org>
2024-08-29 11:35:33 +0200

    trying to fix 3rd party opener states

--- components/gatepro/gatepro.h
+++ components/gatepro/gatepro.h
@@ -17,10 +17,10 @@ enum GateProCmd : uint8_t {
 };  
 
 const std::map<GateProCmd, const char*> GateProCmdMapping = {
-  {GATEPRO_CMD_OPEN, "FULL OPEN;src=P00287D7\\r\\n"},
-  {GATEPRO_CMD_CLOSE, "FULL CLOSE;src=P00287D7\\r\\n"},
-  {GATEPRO_CMD_STOP, "STOP;src=P00287D7\\r\\n"},
-  {GATEPRO_CMD_READ_STATUS, "RS;src=P00287D7\\r\\n"}
+  {GATEPRO_CMD_OPEN, "FULL OPEN;src=P00287D7"},
+  {GATEPRO_CMD_CLOSE, "FULL CLOSE;src=P00287D7"},
+  {GATEPRO_CMD_STOP, "STOP;src=P00287D7"},
+  {GATEPRO_CMD_READ_STATUS, "RS;src=P00287D7"}
 };
 
 
