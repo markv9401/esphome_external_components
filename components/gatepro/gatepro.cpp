52ff263b553745842edf62ad570b696b8cc1b1f8
unknown <mark@rowra.org>
2024-08-29 11:55:15 +0200

    trying to fix 3rd party opener states

--- components/gatepro/gatepro.cpp
+++ components/gatepro/gatepro.cpp
@@ -302,13 +302,10 @@ void GatePro::loop() {
 
   // send first in queue UART cmd
   if (this->tx_queue.size()) {
-    //this->write_str(this->tx_queue.front());
-    std::string temp = this->tx_queue.front();
-    std::vector<uint8_t> cmdrn(temp.begin(), temp.end());
-    //cmdrn.push_back(0x13);
-    //cmdrn.push_back(0x10);
-    this->write_array(cmdrn);
-    //ESP_LOGD(TAG, "UART TX: %s", this->tx_queue.front());
+    this->write_str(this->tx_queue.front());
+    this->write_byte(0x13);
+    this->write_byte(0x10);
+    ESP_LOGD(TAG, "UART TX: %s", this->tx_queue.front());
     this->tx_queue.pop();
   }
 }
