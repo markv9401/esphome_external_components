fbb8b26068af57e9e4c5aac7bc1864502e314f01
Mark <mark@rowra.org>
2024-08-30 11:39:45 +0200

    code cleanup

--- components/gatepro/gatepro.h
+++ components/gatepro/gatepro.h
@@ -5,6 +5,7 @@
 #include "esphome/components/uart/uart.h"
 #include "esphome/components/cover/cover.h"
 #include "esphome/components/sensor/sensor.h"
+#include "helpers.h"
 
 namespace esphome {
 namespace gatepro {
