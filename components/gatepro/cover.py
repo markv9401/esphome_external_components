04dd44f5b123b4be4eb5a365ce61386d800e600a
unknown <mark@rowra.org>
2024-10-09 14:17:15 +0200

    btn

--- components/gatepro/cover.py
+++ components/gatepro/cover.py
@@ -34,6 +34,5 @@ async def to_code(config):
     await cover.register_cover(var, config)
     await uart.register_uart_device(var, config)
 
-    await select.register_select(var, config, options=["1,", "4"])
-    #sel_speed = await select.new_select(config[CONF_SEL_SPEED], options=["1", "4"])
-    #cg.add(var.set_sel_speed(sel_speed))
+    sel_speed = await select.new_select(config[CONF_SEL_SPEED], options=["1", "4"])
+    cg.add(var.set_sel_speed(sel_speed))
