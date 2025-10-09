f534a6658ceec4a8006c351a503960b4992c721e
Mark <mark@rowra.org>
2025-10-09 16:22:33 +0000

    code tidied up

--- components/gatepro/gatepro.cpp
+++ components/gatepro/gatepro.cpp
@@ -8,193 +8,188 @@ namespace gatepro {
 ////////////////////////////////////
 static const char* TAG = "gatepro";
 
-
 ////////////////////////////////////////////
 // Helper / misc functions
 ////////////////////////////////////////////
 void GatePro::queue_gatepro_cmd(GateProCmd cmd) {
-  this->tx_queue.push(GateProCmdMapping.at(cmd));
+   this->tx_queue.push(GateProCmdMapping.at(cmd));
 }
 
 void GatePro::publish() {
-    // publish on each tick
-    // might have to comment out this one
-    if (this->position_ == this->position) {
+   // publish on each tick
+   // might have to comment out this one
+   if (this->position_ == this->position) {
       return;
-    }
+   }
 
-    this->position_ = this->position;
-    this->publish_state();
+   this->position_ = this->position;
+   this->publish_state();
 }
 
 ////////////////////////////////////////////
 // GatePro logic functions
 ////////////////////////////////////////////
 void GatePro::process() {
-  if (!this->rx_queue.size()) {
-    return;
-  }
-  std::string msg = this->rx_queue.front();
-  this->rx_queue.pop();
-
-  //ESP_LOGD(TAG, "UART RX: %s", (const char*)msg.c_str());
-
-  // example: ACK RS:00,80,C4,C6,3E,16,FF,FF,FF\r\n
-  //                          ^- percentage in hex
-  if (msg.substr(0, 6) == "ACK RS") {
-    // status only matters when in motion (operation not finished) 
-    if (this->operation_finished) {
+   if (!this->rx_queue.size()) {
       return;
-    }
-    msg = msg.substr(16, 2);
-    int percentage = stoi(msg, 0, 16);
-    // percentage correction with known offset, if necessary
-    if (percentage > 100) {
-      percentage -= this->known_percentage_offset;
-    }
-    this->position = (float)percentage / 100;
+   }
+   std::string msg = this->rx_queue.front();
+   this->rx_queue.pop();
 
-    return;
-  }
+   //ESP_LOGD(TAG, "UART RX: %s", (const char*)msg.c_str());
+   // example: ACK RS:00,80,C4,C6,3E,16,FF,FF,FF\r\n
+   //                          ^- percentage in hex
+   if (msg.substr(0, 6) == "ACK RS") {
+      // status only matters when in motion (operation not finished) 
+      if (this->operation_finished) {
+         return;
+      }
+      msg = msg.substr(16, 2);
+      int percentage = stoi(msg, 0, 16);
+      // percentage correction with known offset, if necessary
+      if (percentage > 100) {
+         percentage -= this->known_percentage_offset;
+      }
+      this->position = (float)percentage / 100;
+      return;
+   }
 
-  // Read param example: ACK RP,1:1,0,0,1,2,2,0,0,0,3,0,0,3,0,0,0,0\r\n"
-  if (msg.substr(0, 6) == "ACK RP") {
-    this->parse_params(msg);
-    return;
-  }
+   // Read param example: ACK RP,1:1,0,0,1,2,2,0,0,0,3,0,0,3,0,0,0,0\r\n"
+   if (msg.substr(0, 6) == "ACK RP") {
+      this->parse_params(msg);
+      return;
+   }
 
-  // Event message from the motor
-  // example: $V1PKF0,17,Closed;src=0001\r\n
-  if (msg.substr(0, 7) == "$V1PKF0") {
-    if (msg.substr(11, 7) == "Opening") {
+   // Event message from the motor
+   // example: $V1PKF0,17,Closed;src=0001\r\n
+   if (msg.substr(0, 7) == "$V1PKF0") {
+      if (msg.substr(11, 7) == "Opening") {
       this->operation_finished = false;
       this->current_operation = cover::COVER_OPERATION_OPENING;
       this->last_operation_ = cover::COVER_OPERATION_OPENING;
       return;
-    }
-    if (msg.substr(11, 6) == "Opened") {
+      }
+   if (msg.substr(11, 6) == "Opened") {
       this->operation_finished = true;
       this->target_position_ = 0.0f;
       this->current_operation = cover::COVER_OPERATION_IDLE;
       return;
-    }
-    if (msg.substr(11, 7) == "Closing") {
+   }
+   if (msg.substr(11, 7) == "Closing") {
       this->operation_finished = false;
       this->current_operation = cover::COVER_OPERATION_CLOSING;
       this->last_operation_ = cover::COVER_OPERATION_CLOSING;
       return;
-    }
-    if (msg.substr(11, 6) == "Closed") {
+   }
+   if (msg.substr(11, 6) == "Closed") {
       this->operation_finished = true;
       this->target_position_ = 0.0f;
       this->current_operation = cover::COVER_OPERATION_IDLE;
       return;
-    }
-    if (msg.substr(11, 7) == "Stopped") {
+   }
+   if (msg.substr(11, 7) == "Stopped") {
       this->target_position_ = 0.0f;
       this->current_operation = cover::COVER_OPERATION_IDLE;
       return;
-    }
+   }
   }
 }
-
 
 ////////////////////////////////////////////
 // Cover component logic functions
 ////////////////////////////////////////////
 void GatePro::control(const cover::CoverCall &call) {
-  if (call.get_stop()) {
-    this->start_direction_(cover::COVER_OPERATION_IDLE);
-    return;
-  }
-
-  if (call.get_position().has_value()) {
-    auto pos = *call.get_position();
-    if (pos == this->position) {
+   if (call.get_stop()) {
+      this->start_direction_(cover::COVER_OPERATION_IDLE);
       return;
-    }
-    auto op = pos < this->position ? cover::COVER_OPERATION_CLOSING : cover::COVER_OPERATION_OPENING;
-    this->target_position_ = pos;
-    this->start_direction_(op);
-  }
+   }
+
+   if (call.get_position().has_value()) {
+      auto pos = *call.get_position();
+      if (pos == this->position) {
+         return;
+      }
+      auto op = pos < this->position ? cover::COVER_OPERATION_CLOSING : cover::COVER_OPERATION_OPENING;
+      this->target_position_ = pos;
+      this->start_direction_(op);
+   }
 }
 
 void GatePro::start_direction_(cover::CoverOperation dir) {
-  if (this->current_operation == dir ) {
-    return;
-  }
-
-  switch (dir) {
-    case cover::COVER_OPERATION_IDLE:
-      if (this->operation_finished) {
-        break;
-      }
-      this->queue_gatepro_cmd(GATEPRO_CMD_STOP);
-      break;
-    case cover::COVER_OPERATION_OPENING:
-      this->queue_gatepro_cmd(GATEPRO_CMD_OPEN);
-      break;
-    case cover::COVER_OPERATION_CLOSING:
-      this->queue_gatepro_cmd(GATEPRO_CMD_CLOSE);
-      break;
-    default:
+   if (this->current_operation == dir ) {
       return;
-  }
+   }
+
+   switch (dir) {
+      case cover::COVER_OPERATION_IDLE:
+         if (this->operation_finished) {
+            break;
+         }
+         this->queue_gatepro_cmd(GATEPRO_CMD_STOP);
+         break;
+      case cover::COVER_OPERATION_OPENING:
+         this->queue_gatepro_cmd(GATEPRO_CMD_OPEN);
+         break;
+      case cover::COVER_OPERATION_CLOSING:
+         this->queue_gatepro_cmd(GATEPRO_CMD_CLOSE);
+         break;
+      default:
+         return;
+   }
 }
 
 void GatePro::correction_after_operation() {
-    if (this->operation_finished) {
+   if (this->operation_finished) {
       if (this->current_operation == cover::COVER_OPERATION_IDLE &&
-          this->last_operation_ == cover::COVER_OPERATION_CLOSING &&
-          this->position != cover::COVER_CLOSED) {
-        this->position = cover::COVER_CLOSED;
-        return;
+         this->last_operation_ == cover::COVER_OPERATION_CLOSING &&
+         this->position != cover::COVER_CLOSED) {
+         this->position = cover::COVER_CLOSED;
+         return;
       }
 
       if (this->current_operation == cover::COVER_OPERATION_IDLE &&
-          this->last_operation_ == cover::COVER_OPERATION_OPENING &&
-          this->position != cover::COVER_OPEN) {
-        this->position = cover::COVER_OPEN;
+         this->last_operation_ == cover::COVER_OPERATION_OPENING &&
+         this->position != cover::COVER_OPEN) {
+         this->position = cover::COVER_OPEN;
       }
-    }
+   }
 }
 
 void GatePro::stop_at_target_position() {
-  if (this->target_position_ &&
-      this->target_position_ != cover::COVER_OPEN &&
-      this->target_position_ != cover::COVER_CLOSED) {
-    const float diff = abs(this->position - this->target_position_);
-    if (diff < this->acceptable_diff) {
-      this->make_call().set_command_stop().perform();
-    }
-  }
+   if (this->target_position_ &&
+         this->target_position_ != cover::COVER_OPEN &&
+         this->target_position_ != cover::COVER_CLOSED) {
+      const float diff = abs(this->position - this->target_position_);
+      if (diff < this->acceptable_diff) {
+         this->make_call().set_command_stop().perform();
+      }
+   }
 }
 
 ////////////////////////////////////////////
 // UART operations
 ////////////////////////////////////////////
-
 void GatePro::read_uart() {
-    // check if anything on UART buffer
-    int available = this->available();
-    if (!available) {
-        return;
-    }
-    
-    // read the whole buffer into our own buffer
-    // (if there's remainder from previous msgs, concatenate)
-    uint8_t* bytes = new byte[available];
-    this->read_array(bytes, available);
-    this->msg_buff += this->convert(bytes, available);
+   // check if anything on UART buffer
+   int available = this->available();
+   if (!available) {
+      return;
+   }
+   
+   // read the whole buffer into our own buffer
+   // (if there's remainder from previous msgs, concatenate)
+   uint8_t* bytes = new byte[available];
+   this->read_array(bytes, available);
+   this->msg_buff += this->convert(bytes, available);
 
-    // find delimiter, thus a whole msg, send it to processor, then remove from buffer and keep remainder (if any)
-    size_t pos = this->msg_buff.find(this->delimiter);
-    if (pos != std::string::npos) {
-       std::string sub = this->msg_buff.substr(0, pos + this->delimiter_length);
-       this->rx_queue.push(sub);
-       ESP_LOGD(TAG, "UART RX: %s", sub.c_str());
-       this->msg_buff = this->msg_buff.substr(pos + this->delimiter_length, this->msg_buff.length() - pos);
-    }
+   // find delimiter, thus a whole msg, send it to processor, then remove from buffer and keep remainder (if any)
+   size_t pos = this->msg_buff.find(this->delimiter);
+   if (pos != std::string::npos) {
+      std::string sub = this->msg_buff.substr(0, pos + this->delimiter_length);
+      this->rx_queue.push(sub);
+      ESP_LOGD(TAG, "UART RX: %s", sub.c_str());
+      this->msg_buff = this->msg_buff.substr(pos + this->delimiter_length, this->msg_buff.length() - pos);
+   }
 }
 
 void GatePro::write_uart() {
@@ -238,7 +233,6 @@ std::string GatePro::convert(uint8_t* bytes, size_t len) {
 			res += bytes[i];
 		}
 	}
-
 	//ESP_LOGD(TAG, "%s", res.c_str());
 	return res;
 }
@@ -248,104 +242,107 @@ std::string GatePro::convert(uint8_t* bytes, size_t len) {
 // Paramater functions
 ////////////////////////////////////////////
 void GatePro::set_speed_4() {
-	ESP_LOGD(TAG, "SET SPEEEEEED FOO");
-	this->queue_gatepro_cmd(GATEPRO_CMD_READ_PARAMS);
+   ESP_LOGD(TAG, "SET SPEEEEEED FOO");
+   this->queue_gatepro_cmd(GATEPRO_CMD_READ_PARAMS);
 
-	this->paramTaskQueue.push([this](){
-			ESP_LOGD(TAG, "HALÓÓÓÓ ZSÓÓÓTI");
-			this->params[4] = 4;
-			this->write_params(); });
+   this->paramTaskQueue.push(
+      [this](){
+         ESP_LOGD(TAG, "HALÓÓÓÓ ZSÓÓÓTI");
+         this->params[4] = 4;
+         this->write_params();
+      });
 }
 
 void GatePro::parse_params(std::string msg) {
-	this->params.clear();
-	msg = msg.substr(9, 33);
-	size_t start = 0;
-	size_t end;
+   this->params.clear();
+   // example: ACK RP,1:1,0,0,1,2,2,0,0,0,3,0,0,3,0,0,0,0\r\n"
+   //                   ^-9  
+   msg = msg.substr(9, 33);
+   size_t start = 0;
+   size_t end;
 
-	while((end = msg.find(',', start)) != std::string::npos) {
-		this->params.push_back(stoi(msg.substr(start, end - start)));
-		start = end + 1;
-	}
-	this->params.push_back(stoi(msg.substr(start)));
+   // efficiently split on ','
+   while((end = msg.find(',', start)) != std::string::npos) {
+      this->params.push_back(stoi(msg.substr(start, end - start)));
+      start = end + 1;
+   }
+   this->params.push_back(stoi(msg.substr(start)));
 
-	ESP_LOGD(TAG, "Parsed current params:", this->params.size());
-	for (size_t i = 0; i < this->params.size(); ++i) {
-		ESP_LOGD(TAG, "  [%zu] = %d", i, this->params[i]);
-	}
+   ESP_LOGD(TAG, "Parsed current params:", this->params.size());
+   for (size_t i = 0; i < this->params.size(); ++i) {
+      ESP_LOGD(TAG, "  [%zu] = %d", i, this->params[i]);
+   }
 
-	while (!this->paramTaskQueue.empty()) {
-		auto task = this->paramTaskQueue.front();
-		this->paramTaskQueue.pop();
-		task();
-	}
+   // write new params if any task is up
+   while (!this->paramTaskQueue.empty()) {
+      auto task = this->paramTaskQueue.front();
+      this->paramTaskQueue.pop();
+      task();
+   }
 }
 
 void GatePro::write_params() {
-	std::string msg = "WP,1:";
-	for (size_t i = 0; i < this->params.size(); i++) {
-		msg += to_string(this->params[i]);
-		if (i != this->params.size() -1) {
-			msg += ",";
-		}
-	}
-	//msg += ";src=P00287D7\r\n";
-	msg += "\\r\\n";
-	ESP_LOGD(TAG, "BUILT PARAMS: %s", msg.c_str());
-	this->tx_queue.push(msg.c_str());
+   std::string msg = "WP,1:";
+   for (size_t i = 0; i < this->params.size(); i++) {
+      msg += to_string(this->params[i]);
+      if (i != this->params.size() -1) {
+         msg += ",";
+      }
+   }
+   msg += ";src=P00287D7\r\n";
+   ESP_LOGD(TAG, "BUILT PARAMS: %s", msg.c_str());
+   this->tx_queue.push(msg.c_str());
 }
 
 ////////////////////////////////////////////
 // Component functions
 ////////////////////////////////////////////
 cover::CoverTraits GatePro::get_traits() {
-	auto traits = cover::CoverTraits();
-
-	traits.set_is_assumed_state(false);
-	traits.set_supports_position(true);
-	traits.set_supports_tilt(false);
-	traits.set_supports_toggle(true);
-	traits.set_supports_stop(true);
-	return traits;
+   auto traits = cover::CoverTraits();
+   traits.set_is_assumed_state(false);
+   traits.set_supports_position(true);
+   traits.set_supports_tilt(false);
+   traits.set_supports_toggle(true);
+   traits.set_supports_stop(true);
+   return traits;
 }
 
 void GatePro::setup() {
-	ESP_LOGD(TAG, "Setting up GatePro component..");
-	this->last_operation_ = cover::COVER_OPERATION_CLOSING;
-	this->current_operation = cover::COVER_OPERATION_IDLE;
-	this->operation_finished = true;
-	this->queue_gatepro_cmd(GATEPRO_CMD_READ_STATUS);
-	this->blocker = false;
-	this->target_position_ = 0.0f;
-	this->queue_gatepro_cmd(GATEPRO_CMD_READ_PARAMS);
+   ESP_LOGD(TAG, "Setting up GatePro component..");
+   this->last_operation_ = cover::COVER_OPERATION_CLOSING;
+   this->current_operation = cover::COVER_OPERATION_IDLE;
+   this->operation_finished = true;
+   this->queue_gatepro_cmd(GATEPRO_CMD_READ_STATUS);
+   this->target_position_ = 0.0f;
+   this->queue_gatepro_cmd(GATEPRO_CMD_READ_PARAMS);
 
-	if (btn_speed_4) {
-		this->btn_speed_4->add_on_press_callback([this](){this->set_speed_4();});
-	} 
+   // set up buttons
+   if (btn_speed_4) {
+      this->btn_speed_4->add_on_press_callback([this](){this->set_speed_4();});
+   } 
 }
 
 void GatePro::update() {
-    this->publish();
-    this->stop_at_target_position();
+   this->publish();
+   this->stop_at_target_position();
 
-    this->write_uart();
+   this->write_uart();
 
-    if (this->current_operation != cover::COVER_OPERATION_IDLE) {
-        this->queue_gatepro_cmd(GATEPRO_CMD_READ_STATUS);
-    }
+   if (this->current_operation != cover::COVER_OPERATION_IDLE) {
+      this->queue_gatepro_cmd(GATEPRO_CMD_READ_STATUS);
+   }
 
-    this->correction_after_operation();
+   this->correction_after_operation();
 }
 
-
 void GatePro::loop() {
-  // keep reading uart for changes
-  this->read_uart();
-  this->process();
+   // keep reading uart for changes
+   this->read_uart();
+   this->process();
 }
 
 void GatePro::dump_config(){
-    ESP_LOGCONFIG(TAG, "GatePro sensor dump config");
+   ESP_LOGCONFIG(TAG, "GatePro sensor dump config");
 }
 
 }  // namespace gatepro
--- components/gatepro/gatepro.h
+++ components/gatepro/gatepro.h
@@ -13,74 +13,74 @@ namespace esphome {
 namespace gatepro {
 
 enum GateProCmd : uint8_t {
-  GATEPRO_CMD_OPEN,
-  GATEPRO_CMD_CLOSE,
-  GATEPRO_CMD_STOP,
-  GATEPRO_CMD_READ_STATUS,
-  GATEPRO_CMD_READ_PARAMS
+   GATEPRO_CMD_OPEN,
+   GATEPRO_CMD_CLOSE,
+   GATEPRO_CMD_STOP,
+   GATEPRO_CMD_READ_STATUS,
+   GATEPRO_CMD_READ_PARAMS
 };  
 
 const std::map<GateProCmd, const char*> GateProCmdMapping = {
-  {GATEPRO_CMD_OPEN, "FULL OPEN;src=P00287D7\r\n"},
-  {GATEPRO_CMD_CLOSE, "FULL CLOSE;src=P00287D7\r\n"},
-  {GATEPRO_CMD_STOP, "STOP;src=P00287D7\r\n"},
-  {GATEPRO_CMD_READ_STATUS, "RS;src=P00287D7\r\n"},
-  {GATEPRO_CMD_READ_PARAMS, "RP,1:;src=P00287D7\r\n"},
+   {GATEPRO_CMD_OPEN, "FULL OPEN;src=P00287D7\r\n"},
+   {GATEPRO_CMD_CLOSE, "FULL CLOSE;src=P00287D7\r\n"},
+   {GATEPRO_CMD_STOP, "STOP;src=P00287D7\r\n"},
+   {GATEPRO_CMD_READ_STATUS, "RS;src=P00287D7\r\n"},
+   {GATEPRO_CMD_READ_PARAMS, "RP,1:;src=P00287D7\r\n"},
 };
 
 
 class GatePro : public cover::Cover, public PollingComponent, public uart::UARTDevice {
- public:
-  void set_speed_4();
-  esphome::button::Button *btn_speed_4;
-  void set_btn_set_speed_4(esphome::button::Button *btn) { btn_speed_4 = btn; }
+   public:
+      void set_speed_4();
+      esphome::button::Button *btn_speed_4;
+      void set_btn_set_speed_4(esphome::button::Button *btn) { btn_speed_4 = btn; }
 
-  void setup() override;
-  void update() override;
-  void loop() override;
-  void dump_config() override;
-  cover::CoverTraits get_traits() override;
+      void setup() override;
+      void update() override;
+      void loop() override;
+      void dump_config() override;
+      cover::CoverTraits get_traits() override;
 
- protected:
-  // param logic
-  std::vector<int> params;
-  bool params_lock = false;
-  void parse_params(std::string msg);
-  void write_params();
-  std::queue<std::function<void()>> paramTaskQueue;
+   protected:
+      // param logic
+      std::vector<int> params;
+      void parse_params(std::string msg);
+      void write_params();
+      std::queue<std::function<void()>> paramTaskQueue;
 
-  // abstract (cover) logic
-  void control(const cover::CoverCall &call) override;
-  void start_direction_(cover::CoverOperation dir);
+      // abstract (cover) logic
+      void control(const cover::CoverCall &call) override;
+      void start_direction_(cover::CoverOperation dir);
 
-  // device logic
-  std::string convert(uint8_t*, size_t);
-  void process();
-  void queue_gatepro_cmd(GateProCmd cmd);
-  void read_uart();
-  void write_uart();
-  void debug();
-  std::queue<const char*> tx_queue;
-  std::queue<std::string> rx_queue;
-  bool blocker;
-  
-  // sensor logic
-  void correction_after_operation();
-  cover::CoverOperation last_operation_{cover::COVER_OPERATION_OPENING};
-  void publish();
-  void stop_at_target_position();
+      // device logic
+      std::string convert(uint8_t*, size_t);
+      void process();
+      void queue_gatepro_cmd(GateProCmd cmd);
+      void read_uart();
+      void write_uart();
+      void debug();
+      std::queue<const char*> tx_queue;
+      std::queue<std::string> rx_queue;
 
-  // UART parser constants
-  const std::string delimiter = "\\r\\n";
-  const uint8_t delimiter_length = delimiter.length();
-  std::string msg_buff;
+      // sensor logic
+      void correction_after_operation();
+      cover::CoverOperation last_operation_{cover::COVER_OPERATION_OPENING};
+      void publish();
+      void stop_at_target_position();
 
-  const int known_percentage_offset = 128;
-  const float acceptable_diff = 0.05f;
-  float target_position_;
-  float position_;
-  bool operation_finished;
-  cover::CoverCall* last_call_;
+      // UART parser constants
+      const std::string delimiter = "\\r\\n";
+      const uint8_t delimiter_length = delimiter.length();
+      std::string msg_buff;
+
+      // black magic shit..
+      const int known_percentage_offset = 128;
+      // maximum acceptable difference in %
+      const float acceptable_diff = 0.05f;
+      float target_position_;
+      float position_;
+      bool operation_finished;
+      cover::CoverCall* last_call_;
 };
 
 }  // namespace gatepro
