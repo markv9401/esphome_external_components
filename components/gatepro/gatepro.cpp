dfbaa1a37bd27e1f82e1c13ccbf8b64577017e91
root <root@dockerhost.rowra.org>
2025-10-09 17:41:22 +0200

    .

--- components/gatepro/gatepro.cpp
+++ components/gatepro/gatepro.cpp
@@ -205,41 +205,41 @@ void GatePro::write_uart() {
 }
 
 std::string GatePro::convert(uint8_t* bytes, size_t len) {
-  std::string res;
-  char buf[5];
-  for (size_t i = 0; i < len; i++) {
-    if (bytes[i] == 7) {
-      res += "\\a";
-    } else if (bytes[i] == 8) {
-      res += "\\b";
-    } else if (bytes[i] == 9) {
-      res += "\\t";
-    } else if (bytes[i] == 10) {
-      res += "\\n";
-    } else if (bytes[i] == 11) {
-      res += "\\v";
-    } else if (bytes[i] == 12) {
-      res += "\\f";
-    } else if (bytes[i] == 13) {
-      res += "\\r";
-    } else if (bytes[i] == 27) {
-      res += "\\e";
-    } else if (bytes[i] == 34) {
-      res += "\\\"";
-    } else if (bytes[i] == 39) {
-      res += "\\'";
-    } else if (bytes[i] == 92) {
-      res += "\\\\";
-    } else if (bytes[i] < 32 || bytes[i] > 127) {
-      sprintf(buf, "\\x%02X", bytes[i]);
-      res += buf;
-    } else {
-      res += bytes[i];
-    }
-  }
+	std::string res;
+	char buf[5];
+	for (size_t i = 0; i < len; i++) {
+		if (bytes[i] == 7) {
+			res += "\\a";
+		} else if (bytes[i] == 8) {
+			res += "\\b";
+		} else if (bytes[i] == 9) {
+			res += "\\t";
+		} else if (bytes[i] == 10) {
+			res += "\\n";
+		} else if (bytes[i] == 11) {
+			res += "\\v";
+		} else if (bytes[i] == 12) {
+			res += "\\f";
+		} else if (bytes[i] == 13) {
+			res += "\\r";
+		} else if (bytes[i] == 27) {
+			res += "\\e";
+		} else if (bytes[i] == 34) {
+			res += "\\\"";
+		} else if (bytes[i] == 39) {
+			res += "\\'";
+		} else if (bytes[i] == 92) {
+			res += "\\\\";
+		} else if (bytes[i] < 32 || bytes[i] > 127) {
+			sprintf(buf, "\\x%02X", bytes[i]);
+			res += buf;
+		} else {
+			res += bytes[i];
+		}
+	}
 
-  //ESP_LOGD(TAG, "%s", res.c_str());
-  return res;
+	//ESP_LOGD(TAG, "%s", res.c_str());
+	return res;
 }
 
 
@@ -247,85 +247,86 @@ std::string GatePro::convert(uint8_t* bytes, size_t len) {
 // Paramater functions
 ////////////////////////////////////////////
 void GatePro::set_speed_4() {
-  ESP_LOGD(TAG, "SET SPEEEEEED FOO");
-  this->queue_gatepro_cmd(GATEPRO_CMD_READ_PARAMS);
- 
-  this->paramTaskQueue.push([this](){
-    ESP_LOGD(TAG, "HALÓÓÓÓ ZSÓÓÓTI");
-    this->params[4] = 4;
-    this->write_params(); });
+	ESP_LOGD(TAG, "SET SPEEEEEED FOO");
+	this->queue_gatepro_cmd(GATEPRO_CMD_READ_PARAMS);
+
+	this->paramTaskQueue.push([this](){
+			ESP_LOGD(TAG, "HALÓÓÓÓ ZSÓÓÓTI");
+			this->params[4] = 4;
+			this->write_params(); });
 }
 
 void GatePro::parse_params(std::string msg) {
-    this->params.clear();
-    msg = msg.substr(9, 33);
-    size_t start = 0;
-    size_t end;
+	this->params.clear();
+	msg = msg.substr(9, 33);
+	size_t start = 0;
+	size_t end;
 
-    while((end = msg.find(',', start)) != std::string::npos) {
-        this->params.push_back(stoi(msg.substr(start, end - start)));
-	start = end + 1;
-    }
-    this->params.push_back(stoi(msg.substr(start)));
-    
-    ESP_LOGD(TAG, "Parsed current params:", this->params.size());
-    for (size_t i = 0; i < this->params.size(); ++i) {
-        ESP_LOGD(TAG, "  [%zu] = %d", i, this->params[i]);
-    }
+	while((end = msg.find(',', start)) != std::string::npos) {
+		this->params.push_back(stoi(msg.substr(start, end - start)));
+		start = end + 1;
+	}
+	this->params.push_back(stoi(msg.substr(start)));
 
-    while (!this->paramTaskQueue.empty()) {
-       auto task = this->paramTaskQueue.front();
-       this->paramTaskQueue.pop();
-       task();
-    }
+	ESP_LOGD(TAG, "Parsed current params:", this->params.size());
+	for (size_t i = 0; i < this->params.size(); ++i) {
+		ESP_LOGD(TAG, "  [%zu] = %d", i, this->params[i]);
+	}
+
+	while (!this->paramTaskQueue.empty()) {
+		auto task = this->paramTaskQueue.front();
+		this->paramTaskQueue.pop();
+		task();
+	}
 }
 
 void GatePro::write_params() {
-    std::string msg = "WP,1:";
-    for (size_t i = 0; i < this->params.size(); i++) {
-        msg += to_string(this->params[i]);
-	if (i != this->params.size() -1) {
-            msg += ",";
+	std::string msg = "WP,1:";
+	for (size_t i = 0; i < this->params.size(); i++) {
+		msg += to_string(this->params[i]);
+		if (i != this->params.size() -1) {
+			msg += ",";
+		}
 	}
-    }
-    msg += ";src=P00287D7\r\n";
-    ESP_LOGD(TAG, "BUILT PARAMS: %s", msg.c_str());
-    this->tx_queue.push(msg.c_str());
+	//msg += ";src=P00287D7\r\n";
+	msg += "\r\n";
+	ESP_LOGD(TAG, "BUILT PARAMS: %s", msg.c_str());
+	this->tx_queue.push(msg.c_str());
 }
 
 ////////////////////////////////////////////
 // Component functions
 ////////////////////////////////////////////
 cover::CoverTraits GatePro::get_traits() {
-  auto traits = cover::CoverTraits();
+	auto traits = cover::CoverTraits();
 
-  traits.set_is_assumed_state(false);
-  traits.set_supports_position(true);
-  traits.set_supports_tilt(false);
-  traits.set_supports_toggle(true);
-  traits.set_supports_stop(true);
-  return traits;
+	traits.set_is_assumed_state(false);
+	traits.set_supports_position(true);
+	traits.set_supports_tilt(false);
+	traits.set_supports_toggle(true);
+	traits.set_supports_stop(true);
+	return traits;
 }
 
 void GatePro::setup() {
-    ESP_LOGD(TAG, "Setting up GatePro component..");
-    this->last_operation_ = cover::COVER_OPERATION_CLOSING;
-    this->current_operation = cover::COVER_OPERATION_IDLE;
-    this->operation_finished = true;
-    this->queue_gatepro_cmd(GATEPRO_CMD_READ_STATUS);
-    this->blocker = false;
-    this->target_position_ = 0.0f;
-    this->queue_gatepro_cmd(GATEPRO_CMD_READ_PARAMS);
+	ESP_LOGD(TAG, "Setting up GatePro component..");
+	this->last_operation_ = cover::COVER_OPERATION_CLOSING;
+	this->current_operation = cover::COVER_OPERATION_IDLE;
+	this->operation_finished = true;
+	this->queue_gatepro_cmd(GATEPRO_CMD_READ_STATUS);
+	this->blocker = false;
+	this->target_position_ = 0.0f;
+	this->queue_gatepro_cmd(GATEPRO_CMD_READ_PARAMS);
 
-    if (btn_speed_4) {
-        this->btn_speed_4->add_on_press_callback([this](){this->set_speed_4();});
-    } 
+	if (btn_speed_4) {
+		this->btn_speed_4->add_on_press_callback([this](){this->set_speed_4();});
+	} 
 }
 
 void GatePro::update() {
     this->publish();
     this->stop_at_target_position();
-    
+
     this->write_uart();
 
     if (this->current_operation != cover::COVER_OPERATION_IDLE) {
