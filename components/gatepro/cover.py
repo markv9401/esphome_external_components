cbdb28a9f8363937efcc526cb92726886c326c57
root <root@dockerhost.rowra.org>
2025-10-09 09:19:58 +0200

    .

--- components/gatepro/cover.py
+++ components/gatepro/cover.py
@@ -1,6 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import uart, sensor, cover
+from esphome.components import uart, sensor, cover, Cover
 from esphome.const import CONF_ID, ICON_EMPTY, UNIT_EMPTY
 
 DEPENDENCIES = ["uart", "cover"]
