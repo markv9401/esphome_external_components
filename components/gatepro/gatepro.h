e88f2a5b13f238f727aabf87031ed4359262711a
Mark <mark@rowra.org>
2025-10-10 09:14:58 +0000

    .

--- components/gatepro/gatepro.h
+++ components/gatepro/gatepro.h
@@ -98,7 +98,7 @@ class GatePro : public cover::Cover, public PollingComponent, public uart::UARTD
          BtnWithCmd(GateProCmd cmd, button::Button *btn) : cmd(cmd), btn(btn) {};
       };
       std::vector<BtnWithCmd> btns_with_cmds;
-      void set_btn(GateProCmd cmd, button::Button *btn) {
+      void set_btn(std::string, button::Button *btn) {
          this->btns_with_cmds.push_back(BtnWithCmd(find_cmd_by_string(cmd), btn));
       }
 
